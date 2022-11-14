6ccef54a8ad52c64bc59bd58c2ebd39f0af97327
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2022-11-14 09:55:46 -0300

    agregado de loader

--- src/components/ItemDetail/ItemdDetailContainer.jsx
+++ src/components/ItemDetail/ItemdDetailContainer.jsx
@@ -3,34 +3,41 @@ import React, {useState, useEffect} from 'react'
 import { getItem } from '../../services/firestore'
 import { useParams } from 'react-router-dom'
 import ItemDetail from './ItemDetail';
+import Loader from '../Loader/Loader';
 
 
 function ItemdDetailContainer() {
   let [item, setItem] = useState({});
-
-
-
-const { itemId } = useParams(); 
+  const [isLoading, setIsLoading] = useState(true);
+  const { itemId } = useParams(); 
 
 
   useEffect(() => {
-    getItem(itemId).then((respuestaDatos) => setItem(respuestaDatos));
+    getItem(itemId)
+    .then((respuestaDatos) => setItem(respuestaDatos))
+    .finally(()=> setIsLoading(false))
   }, [itemId]);
 
+  if(isLoading){
+    return Loader()
+  }else {
+    return (
+      <div className="detail__container">
+      <ItemDetail 
+            key={item.id}
+            id={item.id}
+            price={item.price}
+            title={item.title}
+            img={item.img}
+            detail={item.detail}
+            stock={item.stock}
+      />
+    </div>
+    )
 
-  return (
-    <div className="detail__container">
-    <ItemDetail 
-          key={item.id}
-          id={item.id}
-          price={item.price}
-          title={item.title}
-          img={item.img}
-          detail={item.detail}
-          stock={item.stock}
-    />
-  </div>
-  )
+  }
+
+
 }
 
 export default ItemdDetailContainer
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { LineWave } from  'react-loader-spinner'
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
+function Loader() {
+  return (
+    <div>
+      <LineWave color="#000"/>
+    </div>
+  )
+}
+
+export default Loader
